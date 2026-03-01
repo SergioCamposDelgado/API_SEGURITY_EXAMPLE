fb6950dfb703c06c7fcc292d94df6cea901544fe
IsraelNarvaezSaavedra <narvaez.saavedra.israel@iescamas.es>
2026-03-01 21:53:36 +0100

    Corregidas las rutas otra vez

--- src/main/java/com/dwes/security/config/SecurityConfiguration.java
+++ src/main/java/com/dwes/security/config/SecurityConfiguration.java
@@ -47,7 +47,7 @@ public class SecurityConfiguration {
 				}).csrf(AbstractHttpConfigurer::disable)
 				.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 
-						.requestMatchers("/api/clima/ciudades", "/api/clima/hoy").permitAll()
+						.requestMatchers("/api/clima/ciudades", "/api/clima/hoy", "/api/v1/auth/**").permitAll()
 
 						.requestMatchers(HttpMethod.POST, "/api/clima/ciudades")
 						.hasAuthority(Role.ROLE_ADMIN.toString())
